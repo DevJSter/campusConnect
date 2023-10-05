cedc9c233a821760424b0162315b7d06c1a4e0e2
Roneet Yadav <116804447+Dev-X-Innovate@users.noreply.github.com>
2023-10-05 19:25:47 +0530

    Enhanced the Home page Components

--- src/components/FindJobsGlimpse.jsx
+++ src/components/FindJobsGlimpse.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import WalkingGirl from '../img/walking_girl.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Lottie from 'react-lottie';
+import animationData from '../img/Animation02.json';
 
 const FindJobsGlimpse = () => {
 
@@ -13,63 +14,49 @@ const FindJobsGlimpse = () => {
     };
 
     return (
+        <section className="md:px-16 grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="">
+  <div className="mb-auto flex-1 flex flex-col items-start justify-center gap-11 md:gap-24 px-8 pb-8 md:p-0">
+    <p className="text-[1.8rem] text-lightPrimary font-semibold"> Why Wait Start Now!</p>
 
-        <section
-            className="md:px-16 grid grid-cols-1 md:grid-cols-2 gap-2 w-full"
-            id=""
-        >
-            <div className="mb-auto flex-1 flex flex-col items-start justify-center gap-11 md:gap-24 px-8 pb-8 md:p-0">
+    <div className='flex flex-col gap-4'>
+      <p className="text-lightModeTextColor text-center font-semibold md:text-justify md:w-[85%] flex-wrap text-xl">
+        Make Your Dreams Work: Find Your Dream Job
+      </p>
+      <p className="text-lightModeTextColor text-center text-sm md:text-justify md:w-[85%] flex-wrap">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+      </p>
+    </div>
 
+    <div className='w-full flex justify-center md:hidden z-30'>
+      <Link to={'/findjob'}>
+        <button className='min-w-210 bg-lightPrimary w-300 p-4 rounded-full text-lightCard cursor-pointer hover:shadow-xl' onClick={handleScrollToTop}>
+          Find Jobs
+        </button>
+      </Link>
+    </div>
 
-                <p className="text-[1.8rem] text-lightPrimary font-semibold"> Why Wait Start Now!</p>
+    <div className='hidden md:flex'>
+      <Link to={'/findjob'}>
+        <button className='min-w-210 bg-lightPrimary w-300 p-4 rounded-full text-lightCard cursor-pointer hover:shadow-xl' onClick={handleScrollToTop}>
+          Find Jobs
+        </button>
+      </Link>
+    </div>
+  </div>
 
-
-                <div className='flex flex-col gap-4'>
-                    <p className="text-lightModeTextColor text-center font-semibold md:text-justify md:w-[85%] flex-wrap text-xl">
-                        Make Your Dreams Work: Find Your Dream Job
-                    </p>
-                    <p className="text-lightModeTextColor text-center text-sm md:text-justify md:w-[85%] flex-wrap">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                    </p>
-                </div>
-
-                <div className='w-full flex justify-center md:hidden z-30'>
-                    <Link to={'/findjob'}>
-                        <button className='min-w-210 bg-lightPrimary w-300 p-4 rounded-full text-lightCard cursor-pointer hover:shadow-xl'
-                            onClick={handleScrollToTop}>
-                            Find Jobs
-                        </button>
-                    </Link>
-                </div>
-
-                <div className='hidden md:flex'>
-                    <Link to={'/findjob'}>
-                        <button
-                            className='min-w-210 bg-lightPrimary w-300 p-4 rounded-full text-lightCard cursor-pointer hover:shadow-xl'
-                            onClick={handleScrollToTop}
-                        >
-                            Find Jobs
-                        </button>
-                    </Link>
-
-                </div>
-
-
-
-            </div>
-            <div className="image-container py-2 flex-1 flex items-center relative">
-                <img
-                    src={WalkingGirl}
-                    className=" ml-auto h-420 w-full lg:w-auto lg:h-650 pb-16"
-                    alt="hero-bg"
-                />
-            </div>
-
-
-
-        </section>
-
+  <div className="image-container py-2 flex-1 flex items-center relative">
+    <Lottie options={{
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+      }} 
+      height={500} 
+      width={500} 
+    />
+  </div>
+</section>
     )
+
 }
 
 export default FindJobsGlimpse
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import HeroSection from '../components/HeroSection'
 import FindJobsGlimpse from '../components/FindJobsGlimpse'
 import Testimonials from '../components/Testimonials'
 import ContactUs from '../components/ContactUs'
+import Timeline from '../components/timeline'
 
 const Home = () => {
   return (
     <div className=' overflow-hidden'>
       <HeroSection />
       <Testimonials />
+      <Timeline/>
       <FindJobsGlimpse />
       <ContactUs/>
     </div>
