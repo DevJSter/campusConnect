6ac74f6467e8700fef1691f97af97987750ce208
Shubham Tiwari <shubht3303@gmail.com>
2023-10-07 23:55:33 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -70,7 +70,8 @@ const NavBar = () => {
 
 
     return (
-        <header className={`${headerClasses}`} fixed top-0 w-full bg-gradient-to-b bg-sky-15 shadow-md z-1000 style={{backgroundImage: 'linear-gradient(to bottom, rgba(0,0,0,0.0.2), rgba(0,0,0,0))',
+        <div className="fixed top-0 w-full bg-gradient-to-b bg-sky-15 shadow-md z-1000 ">
+        <header className={`${headerClasses}`} style={{backgroundImage: 'linear-gradient(to bottom, rgba(0,0,0,0.0.2), rgba(0,0,0,0))',
                                                     backgroundColor: 'bg-sky-15'}} >
         {/* desktop & tablet */}
           <div className="hidden md:flex w-full h-full items-center justify-between">
@@ -349,6 +350,7 @@ const NavBar = () => {
                 </div>
             </div>
         </header>
+        </div>
     );
 };
 
